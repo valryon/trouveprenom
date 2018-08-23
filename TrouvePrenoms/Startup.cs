b06fc8c4d7cb4e6c848d3be8396c2ecb86922e08
Damien Mayance <apps@mayance.io>
2018-08-23 15:05:32 +0200

    Add 2018 file!!!

--- TrouvePrenoms/Startup.cs
+++ TrouvePrenoms/Startup.cs
@@ -63,7 +63,7 @@ namespace TrouvePrenoms
       });
 
       // Load data once on startup
-      string dataFile = Path.Combine(hostingEnvironment.ContentRootPath, "Data", "nat2016.txt");
+      string dataFile = Path.Combine(hostingEnvironment.ContentRootPath, "Data", "nat2017.txt");
       string cacheFile = Path.Combine(hostingEnvironment.ContentRootPath, "Data", "cache.bin");
       PrenomsService.Initialize(dataFile, cacheFile);
     }
